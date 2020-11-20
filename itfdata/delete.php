f412ad8bc32ed0f284c91505ffbd5ac2eed82102
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-20 22:16:06 +0700

    Update delete.php

--- itfdata/delete.php
+++ itfdata/delete.php
@@ -8,12 +8,12 @@ if (mysqli_connect_errno($conn))
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$name = $_POST["name"];
+$comment = $_POST["comment"];
+$link = $_POST["link"];
 
 
-$sql = "DELETE FROM guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+$sql = "DELETE FROM guestbook (Name, Comment, Link) VALUES ("$name", "$comment", "$link")";
 
 
 if (mysqli_query($conn, $sql)) {
