43bd523850306a8c02c8aafc42974509c5732b72
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-20 23:07:06 +0700

    Update edit.php

--- itfdata/edit.php
+++ itfdata/edit.php
@@ -1,9 +1,9 @@
 <?php
 	$conn = mysqli_connect('103.91.205.130', 'roong.pimmada', '^49M6v@FS/TuGR,r', 'roong.pimmada', 3306);
 
-	$id = $_GET['id'];
+	$id = $_GET['ID'];
 
-	$sql = 'SELECT * FROM guestbook WHERE id = '.$id.'';
+	$sql = 'SELECT * FROM guestbook WHERE ID = '.$ID.'';
 	$query = mysqli_query($conn, $sql);
 	if(!$query) {
 		header('Location: show.php');
@@ -19,11 +19,11 @@
 </head>
 <body>
   <form action = "update.php" method = "post" >
-  <input type="text" name="id" value="<?php echo $data['id']; ?>" class="form-control d-none" required><br>
+  <input type="text" name="ID" value="<?php echo $data['ID']; ?>" class="form-control d-none" required hidden><br>
     Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name" value="<?php echo $data['name']; ?>"> <br>
+    <input type="text" name = "name" ID="idName" placeholder="Enter Name" value="<?php echo $data['name']; ?>"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"><?php echo $data['comment']; ?></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" ID="IDComment" placeholder="Enter Comment"><?php echo $data['comment']; ?></textarea><br>  
     Link:<br>
     <input type="text" name = "link" id="idLink" placeholder="Enter Link" value="<?php echo $data['link']; ?>"> <br><br>
     <input type="submit" id="commentBtn">
