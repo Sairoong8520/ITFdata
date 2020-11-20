132a29895b11d60348ba653ae030f73310b76ef6
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-20 20:53:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,62 @@
   
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>LAB ITF</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LabITF</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-    <a href="itfdata/show.php">test</a>
-    <a href="itfdata/form.php">test2</a>
-    <a href="itfdata/insert.php">test3</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5"> 
+                <form action = "insert.php" method = "post" id="CommentForm" >
+                    Name:<br>
+                    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+                    Comment:<br>
+                    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+                    Link:<br>
+                    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+                    <input type="submit" id="commentBtn">
+                  </form>
+                  <form action="delete.php" method="POST" id="deleteForm">
+                    <input type="submit" id="deleteBtn">
+                  </form>
+            <div class="col-sm-3">
+                <?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, '103.91.205.130', 'roong.pimmada', '^49M6v@FS/TuGR,r', 'roong.pimmada', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM Guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+        </div> <!-- Close Row 2 -->
+    </div> <!-- Close container -->
 </body>
 </html>
