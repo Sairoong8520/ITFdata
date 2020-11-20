4b578762c7bfd08c849b46528fd07773d034a4fd
Sairoong8520 <73654933+Sairoong8520@users.noreply.github.com>
2020-11-20 23:54:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM Guestbook');
 ?>
-<table width="600" border="1">
+<table width="1070" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
